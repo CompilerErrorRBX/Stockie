bce5ac3b1ee5271727e0e17808efa5810436adb6
163260 <163260@PLSLAB20752>
2016-10-11 10:47:47 -0500

    Initial Commit

---
+++ Stockie/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Stockie.Startup))]
+namespace Stockie
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
